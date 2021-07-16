58c34919e08c111089f72e44e93235924bc4da6a
ramakholiq22 <ramakholiq22@gmail.com>
2021-07-17 05:14:12 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,16 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ramadlon Nur Kholiq</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Profile Ramadlon Nur Kholiq</title>
+    <link rel="stylesheet" href="frontend/libraries/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="frontend/styles/main.css">
+    <link rel="stylesheet" href="frontend/libraries/retina/retina.js">
+    <link rel="stylesheet" href="frontend/libraries/fontawesome/css/all.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <nav class="navbar navbar-expand-sm fixed-top navbar-dark">
         <a class="navbar-brand" href="index.php">Ramadlon Nur Kholiq</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav">
             <span class="navbar-toggler-icon"></span>
@@ -27,19 +25,19 @@
         </div>
     </nav>
 
-    <header id="header" class="bg-primary py-5 mb-5 text-white text-center">
+    <header id="header" class="py-5 mb-5 text-white text-center">
         <div class="container" style="margin-top: 80px;">
             <div class="row">
                 <div class="col-lg-6 col-12 text-center">
-                    <h1 style="font-size: 50px;">
+                    <h1>
                         Hello, I'am
                     </h1>
-                    <p style="font-size: 25px; font-weight: lighter;">
+                    <p>
                     Ramadlon Nur Kholiq
                     <br>
                     Fullstack Developer
                     </p>
-                    <a class="btn btn-success" href="#project">Let's have a coffee</a>
+                    <a class="btn" href="#project">Let's have a coffee</a>
                 </div>
 
                 <div class="col-lg-6 col-12">
@@ -52,22 +50,22 @@
     <section id="exp">
         <div class="container mt-5">
             <div class="row">
-                <div class="col text-center">
-                    <h3>Expertise</h3>
+                <div class="col-12 text-center">
+                    <h2>Expertise</h2>
                     <div class="row">
-                        <div class="col-md-4 col-12">
-                            <i class="fa fa-pencil" style="font-size: 100px;"></i>
-                            <h4>Designer</h4>
+                        <div class="col-lg-4 col-12">
+                            <i class="fas fa-pencil-alt"></i>
+                            <h5>Designer</h5>
                             <p>I create design simple and beautiful Apps</p>
                         </div>
-                        <div class="col-md-4 col-12">
-                            <i class="fa fa-code" style="font-size: 100px;"></i>
-                            <h4>Developer</h4>
+                        <div class="col-lg-4 col-12">
+                            <i class="fas fa-code" ></i>
+                            <h5>Developer</h5>
                             <p>I like code and enjoy bringing ideas</p>
                         </div>
-                        <div class="col-md-4 col-12">
-                            <i class="fa fa-gears" style="font-size: 100px;"></i>
-                            <h4>Robotic</h4>
+                        <div class="col-lg-4 col-12">
+                            <i class="fas fa-robot"></i>
+                            <h5>Robotic</h5>
                             <p>I build hardware to make work people simple</p>
                         </div>
                     </div>
@@ -76,12 +74,33 @@
         </div>
     </section>
 
-    <section>
-        <div class="container">
-            <h3 class="text-center">Gallery</h3>
+    <section id="service">
+        <div class="container mt-5">
             <div class="row">
-                <div class="col">
-                    
+                <div class="col-12 text-center">
+                    <h2>Services</h2>
+                    <div class="row">
+                        <div class="col-lg-6 col-12">
+                            <i class="fas fa-gamepad"></i>
+                            <h5>Game</h5>
+                            <p>Make your own Game</p>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <i class="fas fa-code"></i>
+                            <h5>Website</h5>
+                            <p>Make your own Website</p>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <i class="fas fa-mobile"></i>
+                            <h5>Mobile Apps</h5>
+                            <p>Make your own apps <br> ios / android</p>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <i class="fas fa-project-diagram"></i>
+                            <h5>Cross Platform Apps</h5>
+                            <p>Make your own apps <br> can run every whare</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -132,42 +151,43 @@
         </div>
     </section>
 
-    <footer id="footer" class="bg-dark mt-5 p-5 text-white">
+    <footer id="footer" class="mt-5 p-5 text-white">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-11 col-lg-11">
+                <div class="col-lg-4 col-12">
+                    <p>Living, learning & leveling up<br>one day at a time</p>
+                </div>
+                <div class="col-lg-6 col-12">
                     <div class="row">
-                        <div class="col">
-                            <p>Living, learning & leveling up<br>one day at a time</p>
+                        <div class="col-12 col-lg-7">
+                            <h5>Office</h5>
+                            <ul class="list-unstyled sitemap">
+                                <li><a target="_blank" href="https://api.whatsapp.com/send?phone=+6281380322145&text=Hallo%20Saya%20ingin%20bekerjasama%20dengan%20Rama."><i class="fab fa-whatsapp"></i> 081380322134</a></li>
+                                <li><i class="fa fa-envelope"></i> ramadlonnurkholiq@gmail.com</li>
+                                <li><i class="fa fa-map"></i> Jl. Sampora Rt 04 Rw 04 No. 49 <br> Kabupaten Bogor, Indonesia</li>
+                            </ul>
                         </div>
-                        <div class="col-12 col-lg-8">
-                            <div class="row">
-                                <div class="col-12 col-lg-7">
-                                    <h5>Office</h5>
-                                    <ul class="list-unstyled sitemap">
-                                        <li><i class="fa fa-envelope"></i> ramadlonnurkholiq@gmail.com</li>
-                                        <li><i class="fa fa-map"></i> Jl. Sampora Rt 04 Rw 04 No. 49 <br> Kabupaten Bogor, Indonesia</li>
-                                    </ul>
-                                </div>
-                                <div class="col-12 col-lg-5">
-                                    <h5>Social Media</h5>
-                                    <ul class="list-unstyled sitemap">
-                                        <li><i class="fa fa-instagram"></i><a href="https://www.instagram.com/rama_kholiq"> Instagram</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                        <div class="col-lg-4 col-12">
+                            <h5>Social Media</h5>
+                            <ul class="list-unstyled sitemap">
+                                <li><a href="https://www.instagram.com/rama_kholiq"><i class="fab fa-instagram"></i> Instagram</a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="row pt-3 text-center">
-                <div class="col">
+                <div class="col-lg-12 col-12">
                     <p> &copy; 2019-2021 Ramadlon Nur Kholiq  • All rights reserved • Love From Kabupaten Bogor</p>
                 </div>
             </div>
         </div>
     </footer>
+
+<!-- JS untuk bootstrap-->
+<script src="frontend/libraries/jquery/jquery-3.6.0.min.js"></script>
+<script src="frontend/libraries/bootstrap/js/bootstrap.js"></script>
 
 <!-- JS untuk Animasi scroll ketika user klik tombol dengan tag <a> -->
 <script>
@@ -186,6 +206,5 @@
         });
     });
 </script>
-    
 </body>
 </html>
