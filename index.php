b890bc13c471e985327f69ae9e9f46512d3c9d20
ramakholiq22 <ramakholiq22@gmail.com>
2021-07-17 05:31:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile Ramadlon Nur Kholiq</title>
+    <link rel="stylesheet" href="frontend/libraries/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="frontend/styles/main.css">
+    <link rel="stylesheet" href="frontend/libraries/retina/retina.js">
+    <link rel="stylesheet" href="frontend/libraries/fontawesome/css/all.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm fixed-top navbar-dark">
+        <a class="navbar-brand" href="index.php">Ramadlon Nur Kholiq</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="myNav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="#header">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="#exp">Expertise</a></li>
+                <li class="nav-item"><a class="nav-link" href="#footer">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <header id="header" class="py-5 mb-5 text-white text-center">
+        <div class="container" style="margin-top: 80px;">
+            <div class="row">
+                <div class="col-lg-6 col-12 text-center">
+                    <h1>
+                        Hello, I'am
+                    </h1>
+                    <p>
+                    Ramadlon Nur Kholiq
+                    <br>
+                    Fullstack Developer
+                    </p>
+                    <a class="btn" href="#project">Let's have a coffee</a>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                    <img src="frontend/images/foto-rama.png" alt="foto-rama" class="float-left img-fluid">
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section id="exp">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h2>Expertise</h2>
+                    <div class="row">
+                        <div class="col-lg-4 col-12">
+                            <i class="fas fa-pencil-alt"></i>
+                            <h5>Designer</h5>
+                            <p>I create design simple and beautiful Apps</p>
+                        </div>
+                        <div class="col-lg-4 col-12">
+                            <i class="fas fa-code" ></i>
+                            <h5>Developer</h5>
+                            <p>I like code and enjoy bringing ideas</p>
+                        </div>
+                        <div class="col-lg-4 col-12">
+                            <i class="fas fa-robot"></i>
+                            <h5>Robotic</h5>
+                            <p>I build hardware to make work people simple</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="service">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h2>Services</h2>
+                    <div class="row">
+                        <div class="col-lg-6 col-12">
+                            <i class="fas fa-gamepad"></i>
+                            <h5>Game</h5>
+                            <p>Make your own Game</p>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <i class="fas fa-code"></i>
+                            <h5>Website</h5>
+                            <p>Make your own Website</p>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <i class="fas fa-mobile"></i>
+                            <h5>Mobile Apps</h5>
+                            <p>Make your own apps <br> ios / android</p>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <i class="fas fa-project-diagram"></i>
+                            <h5>Cross Platform Apps</h5>
+                            <p>Make your own apps <br> can run every whare</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="project" style="margin-top: 50px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4>Start a project</h4>
+                            <hr>
+                            <p>Interested in working together and discuss a project?<br> Let's Have a Coffee!</p>
+                            <div id="mailbox">
+                                <div class="row pt-3">
+                                    <div class="col-12 col-sm-6">
+                                        <div class="form-group">
+                                            <label for="name">Name</label>
+                                            <input type="text" name="name" class="form-control">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12 col-sm-6">
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input type="text" name="email" class="form-control">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <div class="form-group">
+                                            <label for="message">Message</label>
+                                            <textarea name="message" id="message" cols="30" rows="5" class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row pt-3 text-right">
+                                    <div class="col-12">
+                                        <button class="btn btn-success">Send Email</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer id="footer" class="mt-5 p-5 text-white">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-12">
+                    <p>Living, learning & leveling up<br>one day at a time</p>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="row">
+                        <div class="col-12 col-lg-7">
+                            <h5>Office</h5>
+                            <ul class="list-unstyled sitemap">
+                                <li><a target="_blank" href="https://api.whatsapp.com/send?phone=+6281380322145&text=Hallo%20Saya%20ingin%20bekerjasama%20dengan%20Rama."><i class="fab fa-whatsapp"></i> 081380322134</a></li>
+                                <li><i class="fa fa-envelope"></i> ramadlonnurkholiq@gmail.com</li>
+                                <li><i class="fa fa-map"></i> Jl. Sampora Rt 04 Rw 04 No. 49 <br> Kabupaten Bogor, Indonesia</li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4 col-12">
+                            <h5>Social Media</h5>
+                            <ul class="list-unstyled sitemap">
+                                <li><a href="https://www.instagram.com/rama_kholiq"><i class="fab fa-instagram"></i> Instagram</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row pt-3 text-center">
+                <div class="col-lg-12 col-12">
+                    <p> &copy; 2019-2021 Ramadlon Nur Kholiq  • All rights reserved • Love From Kabupaten Bogor</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+<!-- JS untuk bootstrap-->
+<script src="frontend/libraries/jquery/jquery-3.6.0.min.js"></script>
+<script src="frontend/libraries/bootstrap/js/bootstrap.js"></script>
+
+<!-- JS untuk Animasi scroll ketika user klik tombol dengan tag <a> -->
+<script>
+    $(document).ready(function(){
+        $("a").on('click', function(event){
+            if (this.hash !== ""){
+                event.preventDefault();
+                var hash = this.hash;
+
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function(){
+                    window.location.hash = hash;
+                });
+            }
+        });
+    });
+</script>
+</body>
+</html>
